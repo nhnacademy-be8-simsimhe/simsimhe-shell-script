707288401aa4037210c73a982be72a1724bb8d9f
kodongwon <dw9706@naver.com>
2024-12-18 20:26:32 +0900

    fix: api and auth deploy

--- api_deploy.sh
+++ api_deploy.sh
@@ -12,6 +12,20 @@ echo -e "$ip:$SERVICE_PORT에 api-server를 실행시킵니다."
 java -jar -Dserver.port=${SERVICE_PORT} -DLOG_N_CRASH_APP_KEY=${LOG_N_CRASH_APP_KEY} -Dspring.profiles.active=prod ~/target/api-server-0.0.1-SNAPSHOT.jar > ~/log/api_output.log 2> ~/log/api_error.log &
 sleep 10
 
+for retry in {1..10}
+do
+  RESPONSE=$(curl -s http://$ip:$SERVICE_PORT/management/health)
+  PORT_HEALTH=$(echo ${RESPONSE} | grep 'UP' | wc -l)
+  if [ $PORT_HEALTH -eq 1 ];
+  then
+    break
+  else
+    echo -e "$ip:$SERVICE_PORT가 켜져있지 않습니다. 10초 슬립하고 다시 헬스체크를 수행합니다."
+    sleep 5
+  fi
+done
+
+
 echo -e "$ip:$TMP_PORT의 api-server를 종료합니다."
 fuser -s -k -TERM $TMP_PORT/tcp
 sleep 5
--- auth_deploy.sh
+++ auth_deploy.sh
@@ -14,6 +14,19 @@ echo -e "$ip:$SERVICE_PORT에 auth-server를 실행시킵니다."
 java -jar -Dserver.port=${SERVICE_PORT} -DLOG_N_CRASH_APP_KEY=${LOG_N_CRASH_APP_KEY}  ~/target/account-server-0.0.1-SNAPSHOT.jar > ~/log/auth_output.log 2> ~/log/auth_error.log &
 sleep 10
 
+for retry in {1..10}
+do
+  RESPONSE=$(curl -s http://$ip:$SERVICE_PORT/management/health)
+  PORT_HEALTH=$(echo ${RESPONSE} | grep 'UP' | wc -l)
+  if [ $PORT_HEALTH -eq 1 ];
+  then
+    break
+  else
+    echo -e "$ip:$SERVICE_PORT가 켜져있지 않습니다. 10초 슬립하고 다시 헬스체크를 수행합니다."
+    sleep 5
+  fi
+done
+
 echo -e "$ip:$TMP_PORT의 auth-server를 종료합니다."
 fuser -s -k -TERM $TMP_PORT/tcp
 sleep 5
