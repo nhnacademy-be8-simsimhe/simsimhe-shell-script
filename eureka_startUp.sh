64719626c1db9b78ddb3d82d21cfdfe418f9f75b
kodongwon <dw9706@naver.com>
2024-12-18 18:22:33 +0900

    fix: eureka script

--- eureka_startUp.sh
+++ eureka_startUp.sh
@@ -11,7 +11,7 @@ then
 fi
 
 echo -e "$ip:$port에 spring cloud eureka server를 실행합니다."
-java -jar -Dserver.port=${port} -DLOG_N_CRASH_APP_KEY=${LOG_N_CRASH_APP_KEY}  ~/target/eureka-0.0.1-SNAPSHOT.jar &
+java -jar -Dserver.port=${port} -DLOG_N_CRASH_APP_KEY=${LOG_N_CRASH_APP_KEY}  ~/target/eureka-0.0.1-SNAPSHOT.jar > /dev/null 2> ~/log/eureka_error.log &
 sleep 5
 
 for retry in {1..10}
